b5390644e8d2e469629bf1e49a11bd080a84db1c
Rainer Weston <rainerweston@gmail.com>
2021-11-20 21:10:40 +1300

    Moved spawner to it's own scene and refactored code

--- src/scripts/spawner.gd
+++ src/scripts/spawner.gd
@@ -1,35 +1,34 @@
 extends Node
 
+export(bool) var active = false
+export(float) var spawn_time = 1.0
+export(PackedScene) var spawn_object
+export(AABB) onready var bounding_box = bounding_box as AABB
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+#Does this need be unique? Consider moving to a utility singleton
+onready var _rand = RandomNumberGenerator.new()
+onready var _timer = $Timer
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	_rand.randomize()
+	_timer.wait_time = spawn_time
+	_timer.start()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func spawn():
+	if not active:
+		return
+
+	var spawn_instance = spawn_object.instance()
+	
+	spawn_instance.transform.origin = Vector3(
+		_rand.randf_range(bounding_box.position.x, bounding_box.end.x),
+		_rand.randf_range(bounding_box.position.y, bounding_box.end.y),
+		_rand.randf_range(bounding_box.position.z, bounding_box.end.z)
+	)
+	
+	self.add_child(spawn_instance)
 
 
 func _on_Timer_timeout():
-	# print("Spawn start")
-	var rand = RandomNumberGenerator.new()
-	var ballscene = load("res://scenes/ball.tscn")
-	
-	var wall_size = 1
-	
-	var ball = ballscene.instance()
-	rand.randomize()
-	ball.transform.origin = Vector3(
-		rand.randf_range(-wall_size, wall_size),
-		rand.randf_range(0.5, 2),
-		- 2
-	)
-	# print("Done")
-	
-	self.add_child(ball)
-
+	spawn()
