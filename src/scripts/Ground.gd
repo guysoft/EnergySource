827675974a45c7e03d92a8ac6b7ce3c384895d0c
Rainer Weston <rainerweston@gmail.com>
2021-11-30 08:02:34 +1300

    Modified ground scene to add distant mountains

--- src/scripts/Ground.gd
+++ src/scripts/Ground.gd
@@ -5,9 +5,12 @@ export var ground_size = 4.0
 func _ready():
 	pass
 
-func setup_ground(bpm, delay):
+func setup_ground(bpm, delay, color):
 	$GroundShape.mesh.size = Vector2(ground_size,ground_size)
 	var mat = $GroundShape.get_surface_material(0)
 	mat.set_shader_param("uv1_scale",Vector3(ground_size,ground_size,ground_size))
 	var speed = bpm / 60 * ground_size / delay
-	$GroundShape.get_surface_material(0).set_shader_param("speed_mult", speed)
+	mat.set_shader_param("speed_mult", speed)
+	
+	if color is Color:
+		mat.set_shader_param("albedo", color)
