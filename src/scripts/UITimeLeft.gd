ca7273f6587e17279471fd33071dc753b5470f6b
Rainer Weston <rainerweston@gmail.com>
2021-12-01 06:31:26 +1300

    modify time left script to show correct time format

--- src/scripts/UITimeLeft.gd
+++ src/scripts/UITimeLeft.gd
@@ -1,7 +1,8 @@
 extends Label
 
 var time = 0
-
+var minutes_prefix = ""
+var seconds_prefix = ""
 #TODO format correctly with mm:ss
 
 func _ready():
@@ -13,4 +14,18 @@ func _process(delta):
 	var ts = Engine.time_scale
 	var d = delta * (1.0/Engine.time_scale)
 	time = time + d
-	text = String(stepify(time,0.1))
+	
+	var minutes = int(time / 60)
+	var seconds = stepify(time-minutes,0.1)
+	
+	if minutes<10:
+		minutes_prefix = "0"
+	else:
+		minutes_prefix = ""
+	if seconds<10:
+		seconds_prefix = "0"
+	else:
+		seconds_prefix = ""
+	
+	text = minutes_prefix + String(minutes) + ":" + seconds_prefix + String(seconds)
+	#text = String(stepify(time,0.1))
