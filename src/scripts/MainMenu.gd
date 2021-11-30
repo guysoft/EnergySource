2a70e163c12c1568c63ccd56dad2562c0f3e8cfb
Rainer Weston <rainerweston@gmail.com>
2021-12-01 08:52:57 +1300

    menu tweak

--- src/scripts/MainMenu.gd
+++ src/scripts/MainMenu.gd
@@ -2,14 +2,18 @@ extends Spatial
 
 export(Environment) var environment:Environment
 export(AudioStream) var music:AudioStream
+export var start_delay = 1.0
 
 onready var _beat_player = Global.manager()._beatplayer
 onready var _player = Global.manager()._player
 onready var _environment_manager = Global.manager()._environment_manager
 
 func _ready():
-	$AudioStreamPlayer.play()
+	#$AudioStreamPlayer.play()
 	_environment_manager.change_environment(environment)
+	
+	#start_delay = $AudioStreamPlayer.stream.get_length()
+	#yield(get_tree().create_timer(start_delay), "timeout")
 	if _beat_player and music:
 		_beat_player.stream = music
 		_beat_player.play()
