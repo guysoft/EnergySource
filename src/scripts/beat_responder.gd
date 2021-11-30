9861a1b71a901e55d814cae47764e29670257d3f
Rainer Weston <rainerweston@gmail.com>
2021-12-01 02:56:04 +1300

    fix beat responder to work with new structure

--- src/scripts/beat_responder.gd
+++ src/scripts/beat_responder.gd
@@ -8,10 +8,11 @@ export(float) var lerp_value = 0.5
 export(bool) var disabled:=false
 export(float) var set_value = 10.0
 export(float) var response_frequency = 1.0
-export(NodePath) onready var beat_player = get_node(beat_player) as BeatPlayer
+
+#Refs
+onready var _beat_player = Global.manager()._beatplayer
 
 var last_beat = 0 
-
 
 
 func _ready():
@@ -21,8 +22,8 @@ func _ready():
 		set_process(false)
 		return
 	
-	if beat_player:
-		beat_player.connect("beat",self,"_on_beat_detected")
+	if _beat_player:
+		_beat_player.connect("beat",self,"_on_beat_detected")
 
 
 func _process(delta):
@@ -40,6 +41,6 @@ func _on_beat_detected(beat):
 			for material in materials:
 				for key in params.keys():
 					if material.shader.has_param(key):
-						print (key, " ", params[key])
+						#print (key, " ", params[key])
 						material.set_shader_param(key, params[key])
 			last_beat = beat
