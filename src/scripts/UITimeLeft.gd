c3dbdfd090188ae19019e1f67b86b6e319a46972
Rainer Weston <rainerweston@gmail.com>
2021-12-06 09:11:51 +1300

    Refactor and cleanup of time ui

--- src/scripts/UITimeLeft.gd
+++ src/scripts/UITimeLeft.gd
@@ -1,31 +1,30 @@
 extends Label
 
 var time = 0
-var minutes_prefix = ""
-var seconds_prefix = ""
 #TODO format correctly with mm:ss
 
 func _ready():
 	#move to main
-	text = String(stepify(time,0.1))
-	#Events.connect("current_score_updated", self, "_on_current_score_updated")
+	format_time()
+	set_process(false)
+	Events.connect("song_begin", self, "_on_song_begin")
+	Events.connect("song_end", self, "_on_song_end")
+
+func _on_song_begin():
+	set_process(true)
+func _on_song_end():
+	set_process(false)
 
 func _process(delta):
 	var ts = Engine.time_scale
 	var d = delta * (1.0/Engine.time_scale)
 	time = time + d
-	
+	format_time()
+
+func format_time():
 	var minutes = int(time / 60)
-	var seconds = stepify(time-minutes,0.1)
-	
-	if minutes<10:
-		minutes_prefix = "0"
-	else:
-		minutes_prefix = ""
-	if seconds<10:
-		seconds_prefix = "0"
-	else:
-		seconds_prefix = ""
-	
-	text = minutes_prefix + String(minutes) + ":" + seconds_prefix + String(seconds)
-	#text = String(stepify(time,0.1))
+	var seconds = time - minutes*60
+	var miliseconds = (seconds - int(seconds))*100
+
+	var output_string = "%02d:%02d:%02d"
+	text = output_string % [minutes, seconds, miliseconds]
