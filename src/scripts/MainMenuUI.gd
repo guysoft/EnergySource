96b755c37b807fef248e6940f910901120664de0
Rainer Weston <rainerweston@gmail.com>
2021-12-06 09:18:53 +1300

    Non vr menu interactability and a little polish

--- src/scripts/MainMenuUI.gd
+++ src/scripts/MainMenuUI.gd
@@ -8,20 +8,42 @@ var start_pressed := false
 var quit_pressed := false
 
 func _ready():
+	start_pressed = false
 	if start_button:
+		print (start_button)
 		start_button = get_node(start_button)
 	if quit_button:
+		print (quit_button)
 		quit_button = get_node(quit_button)
 	
 	start_button.connect("pressed", self, "_on_start_button_pressed")
 	quit_button.connect("pressed", self, "_on_quit_button_pressed")
+
+func _input(event):
+	if Input.is_key_pressed(KEY_ENTER):
+		_on_start_button_pressed()
+	if Input.is_key_pressed(KEY_ESCAPE):
+		_on_quit_button_pressed()
+#		if event is InputEventKey:
+#			print ("test2")
+#			if event.action=="ui_accept" and event.pressed:
+#				_on_start_button_pressed()
+#			if event.action=="ui_cancel" and event.pressed:
+#				_on_quit_button_pressed()
+
 
 func _on_start_button_pressed():
 	if start_pressed:
 		return
 	$AcceptSound.play()
 	start_pressed = true
-	Global.manager().load_scene(start_scene,"game")
+	Global.manager().load_scene(Global.manager().game_path,"game")
 
 func _on_quit_button_pressed():
+	$BackSound.play()
+	var animation = Global.manager()._transition.get_node("AnimationPlayer") as AnimationPlayer
+	animation.play("fade")
+	yield(animation,"animation_finished")
 	get_tree().quit()
+
+
