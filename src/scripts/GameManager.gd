6386adae780bb656aa8744014108c3d77fbb2db3
Rainer Weston <rainerweston@gmail.com>
2021-12-06 09:28:40 +1300

    Refactor of GameManager.gd, cleaning thing up transitions, xr init and adding game off splash

--- src/scripts/GameManager.gd
+++ src/scripts/GameManager.gd
@@ -1,7 +1,8 @@
 extends Node
 
 # DEVELOPMENT CONSTS
-export (String, "Menu","Game") var debug_start_scene
+export (String, "GGJ2Splash", "Menu","Game") var debug_start_scene
+var splash_path = "res://scenes/GameOffSplash.tscn"
 var menu_path = "res://scenes/MainMenu.tscn"
 var game_path = "res://scenes/Game.tscn"
 
@@ -29,7 +30,7 @@ onready var _beatplayer = $BeatPlayer
 var first = true
 
 
-#var _scorecard = $
+
 
 func _enter_tree():
 	print ("enter tree")
@@ -47,46 +48,44 @@ func _ready():
 	
 	print ("ready begin")
 	if GameVariables.ENABLE_VR:
-		if not initialise_VR():
-			print ("failed to init VR")
-			GameVariables.ENABLE_VR = false #temp, disable on final build
+		if not initialise_OpenXR():
+			print ("failed to init OpenXR")
+			
+			# WebXR set up
+			$Button.connect("pressed", self, "_on_Button_pressed")
+			
+			webxr_interface = ARVRServer.find_interface("WebXR")
+			if webxr_interface:
+				print ("found webxr interface")
+				# WebXR uses a lot of asynchronous callbacks, so we connect to various
+				# signals in order to receive them.
+				webxr_interface.connect("session_supported", self, "_webxr_session_supported")
+				webxr_interface.connect("session_started", self, "_webxr_session_started")
+				webxr_interface.connect("session_ended", self, "_webxr_session_ended")
+				webxr_interface.connect("session_failed", self, "_webxr_session_failed")
+	 
+#				webxr_interface.connect("select", self, "_webxr_on_select")
+#				webxr_interface.connect("selectstart", self, "_webxr_on_select_start")
+#				webxr_interface.connect("selectend", self, "_webxr_on_select_end")
+#
+#				webxr_interface.connect("squeeze", self, "_webxr_on_squeeze")
+#				webxr_interface.connect("squeezestart", self, "_webxr_on_squeeze_start")
+#				webxr_interface.connect("squeezeend", self, "_webxr_on_squeeze_end")
+
+				webxr_interface.is_session_supported("immersive-vr")
+			else:
+				print ("failed to find webxr interface")
+				GameVariables.ENABLE_VR = false #temp, disable on final build
 	else:
 		print("No VR")
 		_left_hand.queue_free()
 		_right_hand.queue_free()
 		
 		
-	# WebXR set up
-	if GameVariables.ENABLE_VR:
-		$Button.connect("pressed", self, "_on_Button_pressed")
-	 
-		webxr_interface = ARVRServer.find_interface("WebXR")
-		if webxr_interface:
-			# WebXR uses a lot of asynchronous callbacks, so we connect to various
-			# signals in order to receive them.
-			webxr_interface.connect("session_supported", self, "_webxr_session_supported")
-			webxr_interface.connect("session_started", self, "_webxr_session_started")
-			webxr_interface.connect("session_ended", self, "_webxr_session_ended")
-			webxr_interface.connect("session_failed", self, "_webxr_session_failed")
-	 
-			webxr_interface.connect("select", self, "_webxr_on_select")
-			webxr_interface.connect("selectstart", self, "_webxr_on_select_start")
-			webxr_interface.connect("selectend", self, "_webxr_on_select_end")
-	 
-			webxr_interface.connect("squeeze", self, "_webxr_on_squeeze")
-			webxr_interface.connect("squeezestart", self, "_webxr_on_squeeze_start")
-			webxr_interface.connect("squeezeend", self, "_webxr_on_squeeze_end")
-	 
-			# This returns immediately - our _webxr_session_supported() method 
-			# (which we connected to the "session_supported" signal above) will
-			# be called sometime later to let us know if it's supported or not.
-			webxr_interface.is_session_supported("immersive-vr")
-			# $Player/ARVROrigin/LeftController.connect("button_pressed", self, "_on_LeftController_button_pressed")
-			# $Player/ARVROrigin/LeftController.connect("button_release", self, "_on_LeftController_button_release")
-
-	
 	
 	match (debug_start_scene):
+		"GGJ2Splash":
+			load_scene(splash_path, "splash")
 		"Menu":
 			load_scene(menu_path, "menu")
 		"Game":
@@ -94,6 +93,8 @@ func _ready():
 	
 	pause_mode = Node.PAUSE_MODE_PROCESS;
 	scenes_holder.pause_mode = Node.PAUSE_MODE_STOP;
+
+	get_tree().paused=false
 	
 	set_process(false)
 
@@ -111,12 +112,13 @@ func add_scene(scene, unique_id: String) -> bool:
 # to the resource or a PackedScene. Returns true on success, false otherwise.
 func load_scene(scene, unique_id: String, additive = false) -> bool:
 	
-	if not first==true:
+	_beatplayer.stop()
+	if first==true:
+		_transition.visible=false
+	else:
 		_transition.get_node("AnimationPlayer").play("fade")
 		yield(_transition.get_node("AnimationPlayer"), "animation_finished")
-	
-	_beatplayer.stop()
-	
+
 	var to_load : PackedScene;
 	
 	if scene is String:
@@ -142,8 +144,9 @@ func load_scene(scene, unique_id: String, additive = false) -> bool:
 	scenes_holder.add_child(new_scene, true)
 	print("Memory: " + str(OS.get_static_memory_peak_usage()))
 	#if not first==true:
-	yield(get_tree().create_timer(1.0), "timeout")
-	_transition.get_node("AnimationPlayer").play_backwards("fade")
+	if not first==true:
+		yield(get_tree().create_timer(1.0), "timeout")
+		_transition.get_node("AnimationPlayer").play_backwards("fade")
 	#yield(transition.get_node("AnimationPlayer"), "animation_finished")
 	
 	first=false
@@ -158,7 +161,123 @@ func unload_scene(unique_id : String):
 		loaded_scenes_map[unique_id].queue_free()
 		loaded_scenes_map.erase(unique_id)
 
-func initialise_VR() -> bool:
+# Web XR functions
+func _webxr_on_select(controller_id: int) -> void:
+	if GameVariables.ENABLE_VR:
+		print("Select: " + str(controller_id))
+	 
+		var controller: ARVRPositionalTracker = webxr_interface.get_controller(controller_id)
+		print (controller.get_orientation())
+		print (controller.get_position())
+ 
+func _webxr_on_select_start(controller_id: int) -> void:
+	if GameVariables.ENABLE_VR:
+#		var controller
+#		if controller_id==1:
+#			controller = _left_hand
+#		else:
+#			controller = _right_hand
+		
+		var controller = webxr_interface.get_controller(controller_id) as ARVRController
+		
+		controller.check_button(JOY_VR_TRIGGER,1)
+		print("Select Start: " + str(controller_id))
+ 
+func _webxr_on_select_end(controller_id: int) -> void:
+	if GameVariables.ENABLE_VR:
+		print("Select End: " + str(controller_id))
+		var controller
+		if controller_id==1:
+			controller = _left_hand
+		else:
+			controller = _right_hand
+		controller.check_button(JOY_VR_TRIGGER,0)
+		
+func _webxr_on_squeeze(controller_id: int) -> void:
+	if GameVariables.ENABLE_VR:
+		print("Squeeze: " + str(controller_id))
+ 
+func _webxr_on_squeeze_start(controller_id: int) -> void:
+	if GameVariables.ENABLE_VR:
+		print("Squeeze Start: " + str(controller_id))
+		var controller
+		if controller_id==1:
+			controller = _left_hand
+		else:
+			controller = _right_hand
+		controller.check_button(JOY_VR_GRIP,1)
+		
+func _webxr_on_squeeze_end(controller_id: int) -> void:
+	if GameVariables.ENABLE_VR:
+		print("Squeeze End: " + str(controller_id))
+		var controller
+		if controller_id==1:
+			controller = _left_hand
+		else:
+			controller = _right_hand
+		controller.check_button(JOY_VR_GRIP,0)
+
+func _webxr_session_supported(session_mode: String, supported: bool) -> void:
+	if GameVariables.ENABLE_VR:
+		if session_mode == 'immersive-vr':
+			vr_web_supported = supported
+
+func begin_webxr():
+	if not vr_web_supported:
+		OS.alert("Your browser doesn't support VR")
+		return
+ 
+	# We want an immersive VR session, as opposed to AR ('immersive-ar') or a
+	# simple 3DoF viewer ('viewer').
+	webxr_interface.session_mode = 'immersive-vr'
+	# 'bounded-floor' is room scale, 'local-floor' is a standing or sitting
+	# experience (it puts you 1.6m above the ground if you have 3DoF headset),
+	# whereas as 'local' puts you down at the ARVROrigin.
+	# This list means it'll first try to request 'bounded-floor', then 
+	# fallback on 'local-floor' and ultimately 'local', if nothing else is
+	# supported.
+	webxr_interface.requested_reference_space_types = 'bounded-floor, local-floor, local'
+	# In order to use 'local-floor' or 'bounded-floor' we must also
+	# mark the features as required or optional.
+	webxr_interface.required_features = 'local-floor'
+	webxr_interface.optional_features = 'bounded-floor'
+ 
+	# This will return false if we're unable to even request the session,
+	# however, it can still fail asynchronously later in the process, so we
+	# only know if it's really succeeded or failed when our 
+	# _webxr_session_started() or _webxr_session_failed() methods are called.
+	if not webxr_interface.initialize():
+		OS.alert("Failed to initialize")
+	else:
+		GameVariables.ENABLE_VR = true
+	return
+
+func _on_Button_pressed() -> void:
+	begin_webxr()
+
+ 
+func _webxr_session_started() -> void:
+	if GameVariables.ENABLE_VR:
+		$Button.visible = false
+		# This tells Godot to start rendering to the headset.
+		get_viewport().arvr = true
+		# This will be the reference space type you ultimately got, out of the
+		# types that you requested above. This is useful if you want the game to
+		# work a little differently in 'bounded-floor' versus 'local-floor'.
+		print ("Reference space type: " + webxr_interface.reference_space_type)
+ 
+func _webxr_session_ended() -> void:
+	if GameVariables.ENABLE_VR:
+		$Button.visible = true
+		# If the user exits immersive mode, then we tell Godot to render to the web
+		# page again.
+		get_viewport().arvr = false
+ 
+func _webxr_session_failed(message: String) -> void:
+	if GameVariables.ENABLE_VR:
+		OS.alert("Failed to initialize: " + message)
+
+func initialise_OpenXR() -> bool:
 	var interface = ARVRServer.find_interface("OpenXR")
 	print (interface)
 	if interface and interface.initialize():
@@ -221,97 +340,3 @@ func _on_openxr_visible_state():
 func _on_openxr_pose_recentered():
 	print("OpenXR pose recentered")
 	
-	
-# Web XR functions
-func _webxr_on_select(controller_id: int) -> void:
-	if GameVariables.ENABLE_VR:
-		print("Select: " + str(controller_id))
-	 
-		var controller: ARVRPositionalTracker = webxr_interface.get_controller(controller_id)
-		print (controller.get_orientation())
-		print (controller.get_position())
- 
-func _webxr_on_select_start(controller_id: int) -> void:
-	if GameVariables.ENABLE_VR:
-		print("Select Start: " + str(controller_id))
- 
-func _webxr_on_select_end(controller_id: int) -> void:
-	if GameVariables.ENABLE_VR:
-		print("Select End: " + str(controller_id))
- 
-func _webxr_on_squeeze(controller_id: int) -> void:
-	if GameVariables.ENABLE_VR:
-		print("Squeeze: " + str(controller_id))
- 
-func _webxr_on_squeeze_start(controller_id: int) -> void:
-	if GameVariables.ENABLE_VR:
-		print("Squeeze Start: " + str(controller_id))
- 
-func _webxr_on_squeeze_end(controller_id: int) -> void:
-	if GameVariables.ENABLE_VR:
-		print("Squeeze End: " + str(controller_id))
-
-
-func _webxr_session_supported(session_mode: String, supported: bool) -> void:
-	if GameVariables.ENABLE_VR:
-		if session_mode == 'immersive-vr':
-			vr_web_supported = supported
- 
-func _on_Button_pressed() -> void:
-	if GameVariables.ENABLE_VR:
-		
-		if not vr_web_supported:
-			OS.alert("Your browser doesn't support VR")
-			return
-	 
-		# We want an immersive VR session, as opposed to AR ('immersive-ar') or a
-		# simple 3DoF viewer ('viewer').
-		webxr_interface.session_mode = 'immersive-vr'
-		# 'bounded-floor' is room scale, 'local-floor' is a standing or sitting
-		# experience (it puts you 1.6m above the ground if you have 3DoF headset),
-		# whereas as 'local' puts you down at the ARVROrigin.
-		# This list means it'll first try to request 'bounded-floor', then 
-		# fallback on 'local-floor' and ultimately 'local', if nothing else is
-		# supported.
-		webxr_interface.requested_reference_space_types = 'bounded-floor, local-floor, local'
-		# In order to use 'local-floor' or 'bounded-floor' we must also
-		# mark the features as required or optional.
-		webxr_interface.required_features = 'local-floor'
-		webxr_interface.optional_features = 'bounded-floor'
-	 
-		# This will return false if we're unable to even request the session,
-		# however, it can still fail asynchronously later in the process, so we
-		# only know if it's really succeeded or failed when our 
-		# _webxr_session_started() or _webxr_session_failed() methods are called.
-		if not webxr_interface.initialize():
-			OS.alert("Failed to initialize")
-			return
- 
-func _webxr_session_started() -> void:
-	if GameVariables.ENABLE_VR:
-		$Button.visible = false
-		# This tells Godot to start rendering to the headset.
-		get_viewport().arvr = true
-		# This will be the reference space type you ultimately got, out of the
-		# types that you requested above. This is useful if you want the game to
-		# work a little differently in 'bounded-floor' versus 'local-floor'.
-		print ("Reference space type: " + webxr_interface.reference_space_type)
- 
-func _webxr_session_ended() -> void:
-	if GameVariables.ENABLE_VR:
-		$Button.visible = true
-		# If the user exits immersive mode, then we tell Godot to render to the web
-		# page again.
-		get_viewport().arvr = false
- 
-func _webxr_session_failed(message: String) -> void:
-	if GameVariables.ENABLE_VR:
-		OS.alert("Failed to initialize: " + message)
- 
-func _on_LeftController_button_pressed(button: int) -> void:
-	if GameVariables.ENABLE_VR:
-		print ("Button pressed: " + str(button))
- 
-func _on_LeftController_button_release(button: int) -> void:
-	if GameVariables.ENABLE_VR:
-		print ("Button release: " + str(button))
