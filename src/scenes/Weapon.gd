3eaaee6eabe219afc7eb12f3171f17b270b9219f
RainerW <rainerweston@gmail.com>
2022-03-31 20:38:02 +1300

    Add test weapon and modify weapon class to be more generic

--- src/scenes/Weapon.gd
+++ src/scenes/Weapon.gd
@@ -4,20 +4,27 @@ class_name Weapon
 
 enum HANDS{LEFT,RIGHT}
 
+#TODO: offload to global so player can change weapon color
 const LEFT_COLOR = Color("f66df4")
 const RIGHT_COLOR = Color("67f8fd")
 
 export(HANDS) var hand
+export(NodePath) var mesh
 
-onready var material = $Blade.get_surface_material(0)
-
+var material
 var velocity_track_point
 
 func _ready():
-	if hand==HANDS.LEFT:
-		material.albedo_color = LEFT_COLOR
-	else:
-		material.albedo_color = RIGHT_COLOR
+	
+	if mesh:
+		mesh = get_node(mesh) as MeshInstance
+		
+		material = mesh.get_surface_material(0)
+		
+		if hand==HANDS.LEFT:
+			material.albedo_color = LEFT_COLOR
+		else:
+			material.albedo_color = RIGHT_COLOR
 		
 
 func get_track_point():
