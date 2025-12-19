dad609c4ce03b479b719af70c8f62c52a0c05fbb
Guy Sheffer <guysoft@gmail.com>
2025-12-20 01:05:54 +0200

    Make game work on quest 2

--- src/scripts/GameVariables.gd
+++ src/scripts/GameVariables.gd
@@ -39,6 +39,43 @@ func _ready():
 		dir = "res:/"
 		project_dir = dir
 		parent_dir = dir
+	elif OS.get_name() == "Android":
+		# On Android/Quest, use the app's external storage directory
+		# This allows music to be sideloaded via adb push
+		print("Android/Quest build")
+		var internal_data_dir = OS.get_user_data_dir()  # /data/data/<package>/files (internal)
+		var external_storage = "/storage/emulated/0"  # Standard Android external storage
+		# External app data dir (accessible via adb push)
+		var external_data_dir = external_storage + "/Android/data/com.tempovr.game/files"
+		
+		project_dir = internal_data_dir
+		
+		# Check multiple possible locations for PowerBeatsVRLevels
+		# Priority order:
+		# 1. External app data dir (adb pushable): /storage/emulated/0/Android/data/com.tempovr.game/files/
+		# 2. Internal app data dir: /data/data/com.tempovr.game/files/
+		# 3. Root of external storage: /storage/emulated/0/
+		print("Checking for PowerBeatsVRLevels...")
+		print("  External data dir: ", external_data_dir)
+		print("  Internal data dir: ", internal_data_dir)
+		
+		if DirAccess.dir_exists_absolute(external_data_dir + "/" + pbvr_levels_foldername):
+			parent_dir = external_data_dir
+			print("Found PowerBeatsVRLevels in external app data dir")
+		elif DirAccess.dir_exists_absolute(internal_data_dir + "/" + pbvr_levels_foldername):
+			parent_dir = internal_data_dir
+			print("Found PowerBeatsVRLevels in internal app data dir")
+		elif DirAccess.dir_exists_absolute(external_storage + "/" + pbvr_levels_foldername):
+			parent_dir = external_storage
+			print("Found PowerBeatsVRLevels in external storage root")
+		else:
+			# Default to external app data dir - user will need to create folder via adb
+			parent_dir = external_data_dir
+			print("PowerBeatsVRLevels not found, defaulting to: ", parent_dir)
+		
+		dir = project_dir
+		self.internal_songs_path = internal_data_dir + "/" + custom_levels_foldername
+		self.custom_songs_path = external_data_dir + "/" + custom_levels_foldername
 	else:
 		if OS.has_feature("editor"):
 			print ("editor build")
